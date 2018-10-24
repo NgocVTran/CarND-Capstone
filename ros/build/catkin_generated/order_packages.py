6e849456dc23b7d1d244c3c3fc7c8205d47cf555
V.Ngoc Tran <linh.nguyen@thermofisher.com>
2018-10-24 05:06:00 +0200

    submit version

---
+++ ros/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/mnt/c/workspace/Udacity/Udacity---Self-Driving-Car-3/CarND-Capstone/ros/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/home/vngoctran/catkin_ws/devel;/opt/ros/kinetic".split(';') if "/home/vngoctran/catkin_ws/devel;/opt/ros/kinetic" != "" else []
--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -23,8 +23,8 @@ as well as to verify your TL classifier.
 TODO (for Yousuf and Aaron): Stopline location for each traffic light.
 '''
 
-LOOKAHEAD_WPS = 100 # Number of waypoints we will publish. You can change this number
-MAX_DECEL = 0.5 # Maximum deceleration
+LOOKAHEAD_WPS = 200 # Number of waypoints we will publish. You can change this number
+MAX_DECEL = 0.2 # Maximum deceleration
 
 class WaypointUpdater(object):
     def __init__(self):
